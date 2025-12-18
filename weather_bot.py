027afce72d5f789376c9bc7ad93e2a22685f5b86
Omarf8 <omarfiguera223@gmail.com>
2025-12-18 17:05:59 -0600

    Testing the weather API

--- weather_bot.py
+++ weather_bot.py
@@ -1,4 +1,5 @@
 import os
+import requests
 import tweepy
 from dotenv import load_dotenv
 from tweepy import OAuthHandler
@@ -6,11 +7,11 @@ from tweepy import OAuthHandler
 # Loads .env variables
 load_dotenv()
 
-bearer_token = os.getenv("BEARER_TOKEN")
-consumer_key = os.getenv("CONSUMER_KEY")
-consumer_secret = os.getenv("CONSUMER_SECRET")
-access_token = os.getenv("ACCESS_TOKEN")
-access_secret = os.getenv("ACCESS_SECRET")
+# bearer_token = os.getenv("BEARER_TOKEN")
+# consumer_key = os.getenv("CONSUMER_KEY")
+# consumer_secret = os.getenv("CONSUMER_SECRET")
+# access_token = os.getenv("ACCESS_TOKEN")
+# access_secret = os.getenv("ACCESS_SECRET")
 
 client = tweepy.Client(
     consumer_key=os.getenv("CONSUMER_KEY"),
@@ -19,10 +20,27 @@ client = tweepy.Client(
     access_token_secret=os.getenv("ACCESS_SECRET")
 )
 
-# Testing to see if I can post tweets to Twitter
-try:
-    tweet = client.create_tweet(text="Testing My Twitter Robot")
-    print("Success! The tweet should have been posted")
-    print(f"ID: {tweet.data["id"]}")
-except Exception as e:
-    print(f"Error: {e}")
+def get_weather(city = "Chicago"):
+    weather_key = os.getenv("OPENWEATHER_API_KEY")
+    url = f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={weather_key}&units=imperial"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+
+        print(f"Name: {data["name"]}")
+        print(f"Temperature: {data["main"]["temp"]} F")
+        print(f"Description: {data["weather"][0]["description"]}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+get_weather()
+
+# # Testing to see if I can post tweets to Twitter
+# try:
+#     tweet = client.create_tweet(text="Testing My Twitter Robot")
+#     print("Success! The tweet should have been posted")
+#     print(f"ID: {tweet.data["id"]}")
+# except Exception as e:
+#     print(f"Error: {e}")
