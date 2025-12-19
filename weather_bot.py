35b21dc07457eb09b8f08f041f1113896f1bc4b3
Omarf8 <omarfiguera223@gmail.com>
2025-12-18 18:52:42 -0600

    Created the get_weather function

--- weather_bot.py
+++ weather_bot.py
@@ -14,33 +14,41 @@ load_dotenv()
 # access_secret = os.getenv("ACCESS_SECRET")
 
 client = tweepy.Client(
-   consumer_key=os.getenv("CONSUMER_KEY"),
-   consumer_secret=os.getenv("CONSUMER_SECRET"),
-   access_token=os.getenv("ACCESS_TOKEN"),
-   access_token_secret=os.getenv("ACCESS_SECRET")
+    consumer_key=os.getenv("CONSUMER_KEY"),
+    consumer_secret=os.getenv("CONSUMER_SECRET"),
+    access_token=os.getenv("ACCESS_TOKEN"),
+    access_token_secret=os.getenv("ACCESS_SECRET")
 )
 
-def get_weather(city = "Chicago"):
-   weather_key = os.getenv("OPENWEATHER_API_KEY")
-   url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_key}&units=imperial"
+def get_weather(city):
+    """Fetch Weather from OpenWeatherMap"""
+    weather_key = os.getenv("OPENWEATHER_API_KEY")
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_key}&units=imperial"
 
-   try:
-       response = requests.get(url)
-       response.raise_for_status()
-       data = response.json()
+     try:
+         response = requests.get(url)
+         response.raise_for_status()
+         data = response.json()
 
-       print(f"Name: {data["name"]}")
-       print(f"Temperature: {data["main"]["temp"]} F")
-       print(f"Description: {data["weather"][0]["description"]}")
-   except Exception as e:
-       print(f"Error: {e}")
+         weather_info = {
+            "city" : data["name"],
+            "temp" : data["main"]["temp"],
+            "description" : data["main"][0]["description"],
+            "humidity" : data["main"]["humidity"],
+            "wind_speed" : data["wind"]["speed"]
+         }
 
-get_weather()
+         print(f"Successfully fetched weather data for {city}")
+         return weather_info
 
-# # Testing to see if I can post tweets to Twitter
-# try:
-#     tweet = client.create_tweet(text="Testing My Twitter Robot")
-#     print("Success! The tweet should have been posted")
-#     print(f"ID: {tweet.data["id"]}")
-# except Exception as e:
-#     print(f"Error: {e}")
+     except Exception as e:
+         print(f"Error: {e}")
+         return None
+
+def post_weather_update(city):
+    print(f"Getting Weather for {city}...")
+
+    weather = get_weather(city)
+
+if __name__ == "__main__":
+    post_weather_update("Chicago")
